89554d99f380a39f6e3a36d3220e21233f1fc3dd
Nathan VanOrman <nathangvo@gmail.com>
2018-07-31 19:59:02 -0600

    Library course component grid finished

--- src/components/library/LibraryCourse.js
+++ src/components/library/LibraryCourse.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react'
 
 import Icon from '../icon';
 import Arrow from '../schedule/arrow';
+import Action from '../action';
 
 class LibraryCourse extends Component {
-  render() {
-    return (
-      <div className="library-course">
-        <div className="library-course__title-arrow">
+    render() {
+      return (
+        <div className="library-course">
+          <div className="library-course__title-check">      
             <label className="library-course__title">Problem Solving</label>
-            { Icon('fas fa-check', 'library-course__icon') }
+            {Icon("fas fa-check", "library-course__icon")}
+          </div>
+          <Arrow className="library-course__arrow" />
+          <Action className="library-course__action"/>
+          <div className="library-course__description">
+            <label>Course Description</label>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              fringilla facilisis mi, non pellentesque metus consectetur vel.
+              Quisque dictum, lectus vitae dignissim tincidunt, sapien nibh
+              placerat diam, quis blandit enim nulla in felis.
+            </p>
+          </div>
         </div>
-        <Arrow className="library-course__arrow"/>
-        {/* action button component */}
-            <div className="library-course__description">
-                <label>Course Description</label>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non turpis eleifend, varius urna eget, congue velit. Aliquam gravida venenatis tellus</p>
-            </div>
-      </div>
-    )
+      );
+    }
   }
-}
-
-
-export default LibraryCourse;
+  
+  export default LibraryCourse;
